a669bfb16a36625f211ee7ca8b4c13fcc3159801
Celtuzed <76615478+Celtuzed@users.noreply.github.com>
2021-02-03 18:45:44 +0300

    Отредактировал табуляцию

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ from dotenv import load_dotenv
 def get_shorten_link(bitly_token, long_url_or_bitlink):
     url = "https://api-ssl.bitly.com/v4/shorten"
     authorization_data = {
-      "Authorization": "Bearer {}".format(bitly_token)
+        "Authorization": "Bearer {}".format(bitly_token)
     }
     data = {
-      "long_url": long_url_or_bitlink
+        "long_url": long_url_or_bitlink
     }
     response = requests.post(url, headers=authorization_data, json=data)
     if response.ok:
@@ -24,7 +24,7 @@ def get_shorten_link(bitly_token, long_url_or_bitlink):
 def get_count_clicks(bitly_token, netloc_and_path):
     url = "https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary".format(netloc_and_path)
     authorization_data = {
-      "Authorization": "Bearer {}".format(bitly_token)
+        "Authorization": "Bearer {}".format(bitly_token)
     }
     response = requests.get(url, headers=authorization_data)
     if response.ok:
@@ -36,7 +36,7 @@ def get_count_clicks(bitly_token, netloc_and_path):
 def check_bitlink(bitly_token, netloc_and_path):
     url = "https://api-ssl.bitly.com/v4/bitlinks/{}".format(netloc_and_path)
     authorization_data = {
-      "Authorization": "Bearer {}".format(bitly_token)
+        "Authorization": "Bearer {}".format(bitly_token)
     }
     response = requests.get(url, headers=authorization_data)
     return response.ok
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     logging.error('Ссылка введена неверно!!!')
     bitly_token = os.getenv("BITLY_TOKEN")
     parser = argparse.ArgumentParser(
-    description='Этот код нужен для создания коротких ссылок (битлинков), а также для того чтобы узнать сколько раз перешли по сокращённой ссылке.'
+        description='Этот код нужен для создания коротких ссылок (битлинков), а также для того чтобы узнать сколько раз перешли по сокращённой ссылке.'
     )
     parser.add_argument('url_or_bitlink', help='Битлинк или ссылка')
     args = parser.parse_args()
