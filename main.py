31c7e4ce0af77ec359150b43cd43e79ae5fd03ca
Celtuzed <76615478+Celtuzed@users.noreply.github.com>
2021-02-12 17:13:26 +0300

    Добавил вывод ошибки

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ def get_shorten_link(bitly_token, long_url_or_bitlink):
         "long_url": long_url_or_bitlink
     }
     response = requests.post(url, headers=authorization_data, json=data)
-    if response.ok:
-        short_url = response.json()
-        link = short_url['id']
-        return link
+    response.raise_for_status()
+    short_url = response.json()
+    link = short_url['id']
+    return link
 
 
 def get_count_clicks(bitly_token, netloc_and_path):
@@ -27,10 +27,10 @@ def get_count_clicks(bitly_token, netloc_and_path):
         "Authorization": "Bearer {}".format(bitly_token)
     }
     response = requests.get(url, headers=authorization_data)
-    if response.ok:
-        count_clicks = response.json()
-        clicks = count_clicks['total_clicks']
-        return clicks
+    response.raise_for_status()
+    count_clicks = response.json()
+    clicks = count_clicks['total_clicks']
+    return clicks
 
 
 def check_bitlink(bitly_token, netloc_and_path):
@@ -39,11 +39,13 @@ def check_bitlink(bitly_token, netloc_and_path):
         "Authorization": "Bearer {}".format(bitly_token)
     }
     response = requests.get(url, headers=authorization_data)
+    response.raise_for_status()
     return response.ok
 
 if __name__ == '__main__':
     load_dotenv()
     bitly_token = os.getenv("BITLY_TOKEN")
+
     parser = argparse.ArgumentParser(
         description='Этот код нужен для создания коротких ссылок (битлинков), а также для того чтобы узнать сколько раз перешли по сокращённой ссылке.'
     )
@@ -51,8 +53,14 @@ if __name__ == '__main__':
     args = parser.parse_args()
     long_url_or_bitlink = args.url_or_bitlink
     parsed_url = urlparse(long_url_or_bitlink)
-    netloc_and_path = (parsed_url.netloc + parsed_url.path)
-    if check_bitlink(bitly_token, netloc_and_path):
-        print('Количество переходов по битлинку -', get_count_clicks(bitly_token, netloc_and_path))
-    elif get_shorten_link(bitly_token, long_url_or_bitlink):
-        print('сокращённая cсылка - ', get_shorten_link(bitly_token, long_url_or_bitlink))
+    netloc_and_path = (f"{parsed_url.netloc + parsed_url.path}")
+
+    try:
+        if check_bitlink(bitly_token, netloc_and_path):
+            clicks = get_count_clicks(bitly_token, netloc_and_path)
+            print("Количество переходов по битлинку -", clicks)
+    except requests.exceptions.HTTPError:
+            try:
+                print('сокращённая cсылка - ', get_shorten_link(bitly_token, long_url_or_bitlink))
+            except requests.exceptions.HTTPError as error:
+                print(error)
